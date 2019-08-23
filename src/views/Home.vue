95643737b21c2ae2bc64550ae80570e3a74d5d34
txl <764496056@qq.com>
2019-08-23 16:10:06 +0800

    ddd

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <menu></menu>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Menu from '@/components/Menu.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Menu
+  }
+}
+</script>
