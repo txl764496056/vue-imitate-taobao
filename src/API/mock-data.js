33e5c62b6d37442c1356088ecb678281b1ba4635
txl <764496056@qq.com>
2019-09-03 18:45:25 +0800

    1

--- src/API/mock-data.js
+++ src/API/mock-data.js
@@ -5,11 +5,6 @@ import common from '../common.js'
 import global from '../global.js'
 let Random = Mock.Random;
 
-// let collect = 0; //收藏
-// let attention = 0; //关注店铺
-// let track = 0; //足迹
-// let coupon = 4;//优惠卷
-// let nickname = '';
 let {
     collect,
     attention,
